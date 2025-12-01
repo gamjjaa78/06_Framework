4ad12d195129b9df6fc2c4e62db510f931f4a975
gamjjaa78 <prnme7@gmail.com>
2025-12-01 17:41:08 +0900

    todoDetail

---
+++ todoList/src/main/resources/static/js/detail.js
@@ -0,0 +1,18 @@
+//목록으로 버튼 동작(메인페이지로 이동)
+const goToList=document.querySelector("#goToList");
+
+goToList.addEventListener("click", ()=> {
+  location.href="/"; //메인페이지(/)로 요청 GET 방식
+});
+
+//--------------------------
+
+//삭제버튼 클릭시 동작
+const deleteBtn=document.querySelector("#deleteBtn");
+deleteBtn.addEventListener("click", (e)=> {
+  if(confirm("정말 삭제원츄?")){
+    //삭제요청
+    location.href=`.todo/delete?todoNo=${e.target.dataset.todoNo}`;
+  }
+
+});
